84043081cf775f13e75df8446dbb854f298f3287
Mickaël Véril <mika.veril@wanadoo.fr>
2021-06-02 13:43:53 +0200

    Add iDeviceDebugCallBack

---
+++ IDevice.NET.Core/Native/iDevice/iDeviceDebugCallBack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace IDevice.NET.Core.Native.iDevice
+{
+
+    [UnmanagedFunctionPointerAttribute(System.Runtime.InteropServices.CallingConvention.Cdecl)]
+    public delegate void iDeviceDebugCallBack(IntPtr message);
+}
