2bd5d3d4818d744c05e1bf409fdee6b18d5c8b6a
mveril <mika.veril@wanadoo.fr>
2021-08-08 14:09:02 +0200

    AFC2Session need to be public

--- IOSLib.AFC/AFC2Session.cs
+++ IOSLib.AFC/AFC2Session.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace IOSLib.AFC
 {
-    class AFC2Session : AFCSessionBase
+    public class AFC2Session : AFCSessionBase
     {
         private const string AFC2_SERVICE_ID = "com.apple.afc2";
 
-        AFC2Session(IDevice device) : base(device, AFC2_SERVICE_ID) { }
+        public AFC2Session(IDevice device) : base(device, AFC2_SERVICE_ID) { }
 
         public override string RootPath => "/";
     }
