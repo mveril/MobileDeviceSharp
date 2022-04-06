6b2bb1efcf382f884d68377ff156226452b71743
mveril <mika.veril@wanadoo.fr>
2022-04-06 18:03:13 +0200

    Add documentation for CustomMashaler

--- iOSLib/Native/CustomMashaler.cs
+++ iOSLib/Native/CustomMashaler.cs
@@ -5,6 +5,10 @@ using System.Runtime.InteropServices;
 
 namespace IOSLib.Native
 {
+    /// <summary>
+    /// A strong typed base type which implement ICustomMarshaler.
+    /// </summary>
+    /// <typeparam name="T">Type of the object to ashal</typeparam>
     public abstract class CustomMashaler<T> : ICustomMarshaler
     {
         void ICustomMarshaler.CleanUpManagedData(object ManagedObj)
@@ -12,8 +16,10 @@ namespace IOSLib.Native
             CleanUpManagedData((T)ManagedObj);
         }
 
-
-
+        /// <summary>
+        /// Performs necessary cleanup of the managed data when it is no longer needed.
+        /// </summary>
+        /// <param name="managedObj">The managed object to be destroyed.</param>
         public abstract void CleanUpManagedData(T managedObj);
 
         public abstract void CleanUpNativeData(IntPtr pNativeData);
@@ -25,13 +31,23 @@ namespace IOSLib.Native
             return MarshalManagedToNative((T)ManagedObj);
         }
 
+        /// <summary>
+        /// Converts the managed data to unmanaged data.
+        /// </summary>
+        /// <param name="managedObj">The managed object to be converted.</param>
+        /// <returns>A pointer to the COM view of the managed object.</returns>
         public abstract IntPtr MarshalManagedToNative(T managedObj);
-
+        
         object ICustomMarshaler.MarshalNativeToManaged(IntPtr pNativeData)
         {
             return MarshalNativeToManaged(pNativeData);
         }
 
+        /// <summary>
+        /// Converts the unmanaged data to managed data.
+        /// </summary>
+        /// <param name="pNativeData">A pointer to the unmanaged data to be wrapped.</param>
+        /// <returns>An object that represents the managed view of the COM data.</returns>
         public abstract T MarshalNativeToManaged(IntPtr pNativeData);
     }
 }
