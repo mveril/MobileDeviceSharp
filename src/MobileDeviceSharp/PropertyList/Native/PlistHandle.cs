62e4d9e7070ed33d3cb7ff50a26d4f570684f867
Mickaël Véril <mika.veril@wanadoo.fr>
2022-11-27 12:30:26 +0100

    PlistHandle can be released only if parrent is null

---
+++ src/MobileDeviceSharp/PropertyList/Native/PlistHandle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static MobileDeviceSharp.PropertyList.Native.Plist;
+
+namespace MobileDeviceSharp.PropertyList.Native
+{
+    public sealed partial class PlistHandle 
+    {
+        protected override bool CanBeReleased()
+        {
+            return plist_get_parent(this) == Zero;
+        }
+    }
+}
