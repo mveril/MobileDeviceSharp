fd7cc005ceb9c19695bce1f5a5e176df10a6df7f
Mickaël Véril <mika.veril@wanadoo.fr>
2022-12-28 18:04:20 +0100

    Fix implementation of InstallationProxy async operation and use userData to keep the callback static

---
+++ src/MobileDeviceSharp.InstallationProxy/EnumerateOperationStatusContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+using MobileDeviceSharp.PropertyList;
+using MobileDeviceSharp.PropertyList.Native;
+using static MobileDeviceSharp.InstallationProxy.Native.InstallationProxy;
+
+namespace MobileDeviceSharp.InstallationProxy
+{
+    internal class EnumerateOperationStatusContext : OperationStatusContext
+    {
+        public EnumerateOperationStatusContext(ChannelWriter<PlistNode> writer)
+        {
+            Writer = writer;
+        }
+
+        public ChannelWriter<PlistNode> Writer { get; }
+
+        protected override void OnCompleted(PlistHandle command, PlistHandle status) => Writer.Complete();
+        protected override void OnException(InstallationProxyOperationException exception) => Writer.Complete(exception);
+        protected override void OnUpdateProgress(PlistHandle command, PlistHandle status)
+        {
+            instproxy_status_get_current_list(status, out _, out _, out _, out var arrayHandle);
+            if (!arrayHandle.IsInvalid)
+            {
+                    using var array = (PlistArray)PlistNode.From(arrayHandle)!;
+                    foreach (var item in array)
+                    {
+                        Writer.TryWrite(item.Clone());
+                    }
+            }
+        }
+    }
+}
---
+++ src/MobileDeviceSharp.InstallationProxy/OperationStatusContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using MobileDeviceSharp.PropertyList.Native;
+using static MobileDeviceSharp.InstallationProxy.Native.InstallationProxy;
+
+namespace MobileDeviceSharp.InstallationProxy
+{
+    internal abstract class OperationStatusContext
+    {
+        protected abstract void OnUpdateProgress(PlistHandle command, PlistHandle status);
+
+        internal void ReportProgress(PlistHandle command, PlistHandle status)
+        {
+            
+            if (TryGetError(status, out var errorName, out var errorDescription, out var errorCode))
+            {
+                OnExceptionCore(command, errorName, errorDescription, errorCode);
+            }
+            OnUpdateProgress(command, status);
+            if (IsComplete(status))
+            {
+                OnCompleted(command, status);
+            }
+        }
+
+        private static bool TryGetError(PlistHandle status, out string errorName, out string errorDescription, out ulong errorCode)
+        {
+            instproxy_status_get_error(status, out errorName, out errorDescription, out errorCode);
+            return errorName is not null;
+        }
+
+        protected virtual void OnExceptionCore(PlistHandle command, string errorName, string errorDescription, ulong  errorCode)
+        {
+            OnException(new InstallationProxyOperationException(errorName, errorDescription, errorCode));
+        }
+
+        protected abstract void OnException(InstallationProxyOperationException exception);
+
+        protected abstract void OnCompleted(PlistHandle command, PlistHandle status);
+
+        internal static bool IsComplete(PlistHandle status)
+        {
+            instproxy_status_get_name(status, out string statusName);
+            return statusName is not null && statusName.Equals("Complete", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
