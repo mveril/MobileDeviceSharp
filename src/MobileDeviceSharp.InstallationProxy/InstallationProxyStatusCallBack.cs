cf9077d8c5c46c59cb7efd79a77d0d9f20007f3c
Mickaël Véril <mika.veril@wanadoo.fr>
2022-12-28 18:02:39 +0100

    Fix InstallationProxyStatusCallback PlistHandle marshaling.

--- src/MobileDeviceSharp.InstallationProxy/InstallationProxyStatusCallBack.cs
+++ src/MobileDeviceSharp.InstallationProxy/InstallationProxyStatusCallBack.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using MobileDeviceSharp.PropertyList.Native;
 
 namespace MobileDeviceSharp.InstallationProxy.Native
 {
-    public delegate void InstallationProxyStatusCallBack(PlistHandle command, PlistHandle status, IntPtr userData);
+    public delegate void InstallationProxyStatusCallBack([MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(PlistHandleNotOwnedMarshaler))] PlistHandle command, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(PlistHandleNotOwnedMarshaler))] PlistHandle status, IntPtr userData);
 }
